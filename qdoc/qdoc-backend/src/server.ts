6d0c38670fd877b42a936270aa4d1bdc7dd9965f
kelvin beak <kelvinbeaksh@gmail.com>
2025-04-08 22:36:05 +0800

    feat: update backend port number

--- qdoc/qdoc-backend/src/server.ts
+++ qdoc/qdoc-backend/src/server.ts
@@ -2,7 +2,7 @@ import { initDB } from "./utils/db-connection";
 import { Logger } from "./logger";
 import app from "./app";
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 8010;
 
 initDB()
   .catch((err) => Logger.error("Failed connecting to DB.", err));
